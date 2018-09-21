d71bbc7d89ccced44283529f9dcd3663733f0ab5
wangshuaiwu <wangshuaiwu@100tal.com>
2018-09-21 19:58:12 +0800

    222

---
+++ src/ch3/复数.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+/*
+	Go语言提供了两种精度的复数类型：complex64和complex128，分别对应float32和float64两种浮点数精度。内置的complex函数用于构建复数，内建的real和imag函数分别返回复数的实部和虚部
+	在常量算术规则下，一个复数常量可以加到另一个普通数值常量（整数或浮点数、实部或虚部），我们可以用自然的方式书写复数，就像1+2i或与之等价的写法2i+1。
+*/
+func main() {
+	var x complex128 = complex(1, 2) // 1+2i
+	var y complex128 = complex(3, 4) // 3+4i
+	fmt.Println(x*y) // "(-5+10i)"
+	fmt.Println(real(x*y)) // "-5"
+	fmt.Println(imag(x*y)) // "10"
+}
